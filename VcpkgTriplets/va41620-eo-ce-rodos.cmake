1f740e086560bcf28d7eee0688db865720058ee4
Patrick Kappl <patrick.kappl@fantana.at>
2026-02-04 14:05:48 +0100

    Use MinSizeRel flags for Release builds with vcpkg
    The new toolchain file Va416xxMinSizeRelFlagsForRelease.cmake
    effectively makes vcpkg use the MinSizeRel config instead of the Release
    one. This is necessary since on embedded platforms we prefer MinSizeRel
    over Release but vcpkg does not support changing the build type/config
    like that.

--- VcpkgTriplets/va41620-eo-ce-rodos.cmake
+++ VcpkgTriplets/va41620-eo-ce-rodos.cmake
@@ -1,9 +1,15 @@
 set(VCPKG_CRT_LINKAGE static) # Not used on embedded systems but required to be set
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../CMake/Toolchains/Va416xx.cmake")
+# On an embedded platform we want to build the MinSizeRel config instead of the Release one but
+# vcpkg does not support changing that. Therefore, we chainload a custom toolchain file that uses
+# MinSizeRel flags for the Release config.
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE
+    "${CMAKE_CURRENT_LIST_DIR}/../CMake/Toolchains/Va416xxMinSizeRelFlagsForRelease.cmake"
+)
 # Hash all files included by the toolchain file
 set(VCPKG_HASH_ADDITIONAL_FILES
+    "${CMAKE_CURRENT_LIST_DIR}/../CMake/Toolchains/Va416xx.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/../CMake/Platform/Generic-ELF-GNU-C.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/../CMake/Platform/Generic-ELF-GNU-CXX.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/../CMake/Platform/Generic-ELF-GNU.cmake"

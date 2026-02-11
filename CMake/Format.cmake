fb911179983f8a7bc852051ec5562484f8dd4e2a
Patrick Kappl <patrick.kappl@fantana.at>
2026-02-11 16:48:56 +0100

    Fix Format.cmake not failing when sub-scripts fail
    I removed the check of the result variables a while ago because I
    thought it is redundant with the `FATAL_ERROR` messages of the
    sub-scripts. Turns out I was wrong.

--- CMake/Format.cmake
+++ CMake/Format.cmake
@@ -7,8 +7,18 @@ cmake_minimum_required(VERSION 3.14)
 
 set(fix_flag "")
 if(FIX)
-    set(fix_flag "-D FIX=YES")
+    set(fix_flag -D FIX=YES)
 endif()
 
-execute_process(COMMAND "${CMAKE_COMMAND}" ${fix_flag} -P "CMake/FormatCpp.cmake")
-execute_process(COMMAND "${CMAKE_COMMAND}" ${fix_flag} -P "CMake/FormatCMake.cmake")
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" ${fix_flag} -P CMake/FormatCpp.cmake
+    RESULT_VARIABLE cpp_result
+)
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" ${fix_flag} -P CMake/FormatCMake.cmake
+    RESULT_VARIABLE cmake_result
+)
+
+if(NOT cpp_result EQUAL 0 OR NOT cmake_result EQUAL 0)
+    cmake_language(EXIT 1)
+endif()

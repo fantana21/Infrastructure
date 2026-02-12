d82194613b26136870f1147b3311bbd5cac2ac0c
Patrick Kappl <kappl.pa@gmail.com>
2026-02-12 17:00:05 +0100

    Add ClangTidyForCrossCompiling.cmake

---
+++ CMake/ClangTidyForCrossCompiling.cmake
@@ -0,0 +1,19 @@
+# When cross-compiling, clang-tidy needs manual help with the target triple and the include paths to
+# find the correct standard library headers.
+if(CMAKE_CXX_CLANG_TIDY AND CMAKE_CXX_COMPILER MATCHES "arm-none-eabi")
+    list(APPEND CMAKE_CXX_CLANG_TIDY --extra-arg=--target=arm-none-eabi)
+    set(implicit_includes
+        ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES}
+        ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES}
+    )
+    list(REMOVE_DUPLICATES implicit_includes)
+    foreach(include IN LISTS implicit_includes)
+        # Skip GCC's builtin headers because clang does not support the same builtins and
+        # intrinsics. Clang provides its own so-called resource headers for that purpose, which
+        # come bundled with the compiler not the standard library.
+        if(include MATCHES "lib/gcc")
+            continue()
+        endif()
+        list(APPEND CMAKE_CXX_CLANG_TIDY "--extra-arg=-isystem${include}")
+    endforeach()
+endif()

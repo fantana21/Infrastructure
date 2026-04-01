7a40e7d0356a6ed266e7a55fa86c04e6168b9068
Patrick Kappl <patrick.kappl@fantana.at>
2026-04-01 12:28:18 +0200

    Disable PIE in LinuxX86.cmake to suppress text relocation warning
    The linker warns about a relocation in the .text section, caused by
    RODOS. It probably comes from some hand-written assembly code in RODOS
    that is not position independent. This is not a problem because ASLR
    (address space layout randomization) and RELRO (Relocation Read-Only),
    the reasons for wanting position independent code, are irrelevant for
    RODOS targets.

--- CMake/Toolchains/LinuxX86.cmake
+++ CMake/Toolchains/LinuxX86.cmake
@@ -1,4 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 add_compile_options(-m32)
-add_link_options(-m32)
+# The -no-pie option is to suppress linker warnings about relocation in .text sections caused by
+# RODOS. It probably comes from some hand-written assembly code in RODOS that is not position
+# independent.
+add_link_options(-m32 -no-pie)
